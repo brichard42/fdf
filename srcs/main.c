0b11860931cf8de3f68b6e4c5c12389637bcb341
Bastien RICHARD-CANAVAGGIO <brichard@student.42.fr>
2019-02-15 15:23:20 +0100

    yes s

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: brichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 11:10:24 by brichard          #+#    #+#             */
-/*   Updated: 2019/02/15 15:14:55 by brichard         ###   ########.fr       */
+/*   Updated: 2019/02/15 15:20:38 by brichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int		main(void)
 	t_mlx	mlx;
 	int i;
 
-	ft_printf("MAIN | test = %s", "plop");
 	i = 0;
 	if (!(mlx.mlx_ptr = mlx_init()))
 		pexit(E_MLX_INIT);
@@ -40,6 +39,7 @@ int		main(void)
 		i++;
 	}
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
+	ft_printf("MAIN | test = %s", "plop");
 //	get_file(ac, av);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
