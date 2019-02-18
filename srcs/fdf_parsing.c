3baaa2c111d7666425054d3d2f7332d812b57a01
Bastien RICHARD-CANAVAGGIO <brichard@student.42.fr>
2019-02-18 18:08:45 +0100

    aled

--- srcs/fdf_parsing.c
+++ srcs/fdf_parsing.c
@@ -6,55 +6,63 @@
 /*   By: brichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/16 15:28:12 by brichard          #+#    #+#             */
-/*   Updated: 2019/02/16 20:39:07 by brichard         ###   ########.fr       */
+/*   Updated: 2019/02/18 18:07:58 by brichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static int		put_in_point(t_list **begin, char *line,
-								t_point *pt, size_t num_line)
+static int		put_in_tab(t_list *begin, int **file, int num_line)
 {
 	int		i;
-	t_list	*new;
+	int		j;
+	char	*line;
 
-	i = 0;
-	while (*line)
+	line = NULL;
+	if (!(file = ft_memalloc((num_line + 1) * sizeof(int *))))
+		return (-1);
+	i = -1;
+	while (begin)
 	{
-		if (ft_isdigit(*line))
+		line = (char *)begin->content;
+		if (!(file[++i] = ft_memalloc(line * 4)))
 		{
-			pt->x = i;
-			pt->y = num_line;
-			pt->z = ft_atoi(line);
-			if (!(new = ft_lstnew((void *)pt, sizeof(t_point))))
-				return (0);
-			ft_lstadd(begin, new);
-			while (*line && ft_isdigit(*line))
-				++line;
-			++i;
+			//+++++++ TABDEL ICI SINON LEAKS ++++++++
+			return (-1);
 		}
-		++line;
+		j = -1;
+		while (*line)
+		{
+			if (ft_isdigit(*line))
+			{
+				file[i][++j] = ft_atoi(line);
+				while (*line && ft_isdigit(*line))
+					++line;
+			}
+		}
+		begin = begin->next;
 	}
-	return (1);
+	return (0);
 }
 
 static int		put_in_lst(const int fd, t_list **begin)
 {
-	t_point	pt;
 	char	*line;
 	int		num_line;
 	int		ret;
+	t_list	*new;
 
 	line = NULL;
 	num_line = 0;
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		if (!(put_in_point(begin, line, &pt, num_line)))
+		if (!(new = ft_lstnew((void *)line, ft_strlen(line))))
 		{
 			ft_lstdel(begin, ft_del_cont);
 			ft_memdel((void **)&line);
 			return (-1);
 		}
+		ft_lstapp(begin, new);
 		++num_line;
 		ft_memdel((void **)&line);
 	}
@@ -66,16 +74,29 @@ static int		put_in_lst(const int fd, t_list **begin)
 	return (num_line);
 }
 
-int				fdf_parsing(char *av, t_list **begin)
+int				fdf_parsing(char *av, int **file)
 {
-	int		num_lines;
 	int		fd;
+	t_list	*begin;
+	int		num_line;
 
+	begin = NULL;
 	fd = open(av, O_RDONLY);
 	if (fd == -1)
 		return (-1);
-	if ((num_lines = put_in_lst(fd, begin)) == -1)
+	if ((num_line = put_in_lst(fd, &begin)) == -1)
+	{
+		if (close(fd) == -1)
 			return (-1);
+		return (-1);
+	}
+	if (put_in_tab(begin, file, num_line) == -1)
+	{
+		ft_lstdel(&begin, ft_del_cont);
+		if (close(fd) == -1)
+			return (-1);
+		return (-1);
+	}
 	if (close(fd) == -1)
 		return (-1);
 	return (0);
--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: brichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 11:10:24 by brichard          #+#    #+#             */
-/*   Updated: 2019/02/17 19:15:20 by brichard         ###   ########.fr       */
+/*   Updated: 2019/02/18 18:07:59 by brichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#define SCALE 50
 
 static int	pexit(int exit_value)
 {
@@ -19,152 +18,34 @@ static int	pexit(int exit_value)
 	exit(exit_value);
 }
 
-static int	map_len_x(t_list *begin)
-{
-	static int len = 0;
-	
-	if (len == 0)
-		len = ((t_point *)begin->content)->x;
-	return (len);
-}
-
-static int	map_len_y(t_list *begin)
-{
-	static int len = 0;
-	
-	if (len == 0)
-		len = ((t_point *)begin->content)->y;
-	return (len);
-}
-
-static void	init(t_mlx *mlx)
+static void	init_window(t_mlx *mlx)
 {
 	if (!(mlx->mlx_ptr = mlx_init()))
 		pexit(E_MLX_INIT);
 	if (!(mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, W_WIDTH,
 											W_HEIGHT, "MyWindow")))
 		pexit(E_MLX_NEW_WINDOW);
-	mlx->img.data_size = W_WIDTH * W_HEIGHT * 4;
-	if (!(mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, W_WIDTH, W_HEIGHT)))
-	{
-		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
-		pexit(E_MLX_NEW_IMAGE);
-	}
-	mlx->img.data = mlx_get_data_addr(mlx->img.img_ptr, &mlx->img.bpp,
-									&mlx->img.size_l, &mlx->img.endian);
 }
 
-static void bresenham(t_img *img, t_point pt1, t_point pt2)
-{
-	int		ex;
-	int		ey;
-	int		dx;
-	int		dy;
-	int		dex;
-	int		dey;
-	int		xincr;
-	int		yincr;
-	int		i;
-
-	ex = abs(pt2.x - pt1.x);
-	ey = abs(pt2.y - pt1.y);
-	dx = 2 * ex;
-	dy = 2 * ey;
-	dex = ex;
-	dey = ey;
-	xincr = 1;
-	yincr = 1;
-	i = 0;
-	if (pt1.x > pt2.x)
-		xincr = -1;
-	if (pt1.y  > pt2.y)
-		yincr = -1;
-	if (dex > dey)
-	{
-		while (i <= dex)
-		{
-			image_pixel_put(img, pt1.x, pt1.y, (pt1.z > 0 && pt2.z > 0 ? 0x00FF00 : 0x0000DD));
-			++i;
-			pt1.x += xincr;
-			ex -= dy;
-			if (ex < 0)
-			{
-				pt1.y += yincr;
-				ex += dx;
-			}
-		}
-	}
-	else if (dex < dey)
-	{
-		while (i <= dey)
-		{
-			image_pixel_put(img, pt1.x, pt1.y, (pt1.z > 0 && pt2.z > 0 ? 0x00FF00 : 0x0000DD));
-			++i;
-			pt1.y += yincr;
-			ey -= dx;
-			if (ey < 0)
-			{
-				pt1.x += xincr;
-				ey += dy;
-			}
-		}
-	}
-}
-
-static void	fill_image(t_list *begin, t_img *img)
-{
-	int		i;
-	int		lim_x;
-	t_list	*next_bottom_elem;
-
-	i = 0;
-	lim_x= 1 + map_len_x(begin) / SCALE;
-	while (begin)
-	{
-		if (begin->next && (i % lim_x) != lim_x - 1)
-			bresenham(img, (*(t_point *)begin->content), (*(t_point *)begin->next->content));
-		if ((next_bottom_elem = ft_lstnelem(begin, lim_x + 1)))
-			bresenham(img, (*(t_point *)begin->content), (*(t_point *)next_bottom_elem->content));
-		image_pixel_put(img, ((t_point *)begin->content)->x, ((t_point *)begin->content)->y, (((t_point *)begin->content)->z > 0 ? 0xFFFFFF :  0xFF0000));
-		++i;
-		begin = begin->next;
-	}
-}
-
-static void	centerise(t_list *begin)
-{
-	while (begin)
-	{
-		((t_point *)begin->content)->x += W_WIDTH / 2 - map_len_x(begin) / 2;
-		((t_point *)begin->content)->y +=  + W_HEIGHT / 2 - map_len_y(begin) / 2;
-		begin = begin->next;
-	}
-}
-
-static int	task_manager(t_list *begin)
+static int	task_manager(char **file)
 {
 	t_mlx	mlx;
 
-	init(&mlx);
-	apply_scale_on_lst(begin, SCALE);
-	map_len_y(begin);
-	centerise(begin);
-	fill_image(begin, &mlx.img);
-	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
+	(void)file;
+	init_window(&mlx);
 	mlx_loop(mlx.mlx_ptr);
-	mlx_destroy_window(mlx.mlx_ptr, mlx.win_ptr);
 	return (0);
 }
 
 int			main(int ac, char **av)
 {
-	t_list	*begin;
+	int		**file;
 
-	begin = NULL;
+	file = NULL;
 	if (ac != 2)
 		return (0);
-	if (fdf_parsing(av[1], &begin) == -1)
+	if ((fdf_parsing(av[1], file)) == -1)
 		pexit(E_FDF_PARSING);
-	task_manager(begin);
+	task_manager(file);
 	return (0);
 }
