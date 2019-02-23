ad98aef714d7e3e50ef095194478de107cbbe867
brichard42 <brichard42@github.com>
2019-02-23 10:36:18 +0100

    modif des keycodes, working f
    f
    
    cccc
    ''

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,26 @@
 /*   By: brichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 11:10:24 by brichard          #+#    #+#             */
-/*   Updated: 2019/02/23 02:15:20 by brichard         ###   ########.fr       */
+/*   Updated: 2019/02/23 10:32:38 by brichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+# define KEY_A 97
+# define KEY_Z 122
+# define KEY_E 101
+# define KEY_R 114
+# define KEY_T 116
+# define KEY_Y 121
+# define KEY_U 117
+# define KEY_I 105
+# define KEY_O 111
+# define KEY_P 112
+# define KEY_UP 65364
+# define KEY_DOWN 65362
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# define KEY_ESC 65307
 
 /*
 parsing - FICHIER = CARRER OU RECTANGLE SINON INVALIDE
@@ -24,7 +39,7 @@ int			do_key_release(int keycode, void *param)
 	t_mlx	*env;
 
 	env = (t_mlx *)param;
-	if (keycode == 53)
+	if (keycode == KEY_ESC)
 	{
 		mlx_destroy_window(env->mlx_ptr, env->win_ptr);
 		//++++++ FREE LES TRUCS PLEASE ++++++//
@@ -37,42 +52,41 @@ int			do_key(int keycode, void *param)
 {
 	t_mlx	*env;
 
-	(void)keycode;
 	env = (t_mlx *)param;
 	ft_bzero(env->img.data, W_HEIGHT * env->img.size_l);
-	if (keycode == 123)
+	if (keycode == KEY_LEFT)
 	{
 		env->file.x_move = -5;
 		do_maths(&env->file, "mx");
 	}
-	if (keycode == 124)
+	if (keycode == KEY_RIGHT)
 	{
 		env->file.x_move = 5;
 		do_maths(&env->file, "mx");
 	}
-	if (keycode == 126)
+	if (keycode == KEY_DOWN)
 	{
 		env->file.y_move = -5;
 		do_maths(&env->file, "my");
 	}
-	if (keycode == 125)
+	if (keycode == KEY_UP)
 	{
 		env->file.y_move = 5;
 		do_maths(&env->file, "my");
 	}
-	if (keycode == 23/*34*/)
+	if (keycode == KEY_I)
 		do_maths(&env->file, "r");
-	if (keycode == 116)
+	if (keycode == KEY_Z)
 	{
 		env->file.zoom = 1.01;
 		do_maths(&env->file, "z");
 	}
-	if (keycode == 24/*121*/)
+	if (keycode == KEY_E)
 	{
 		env->file.zoom = 0.9;
 		do_maths(&env->file, "z");
 	}
-	if (keycode == 49)
+	if (keycode == KEY_P)
 	{
 		scale_view(&env->file);
 		center_view(&env->file);
